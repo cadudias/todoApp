e60dd23fb9dfb137b2ee857d8ef5736c2a66b1b1
Ricardo Dias <cadu.hd@gmail.com>
2020-12-14 23:02:52 -0300

    adicionado componente de input de texto de todos

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, FlatList, Text } from 'react-native';
 
 import Header from './components/Header'
+import InputTodo from './components/InputTodo'
 
-const App: () => React$Node = () => {
+const App = () => {
+
+  const [ todos, setTodos ] = useState([]);
+
+  const adicionarTodo = (textoTodo) => {
+    setTodos(todosAtuais => [
+      ...todosAtuais, 
+      {
+        id: Math.random().toString(), 
+        value: textoTodo 
+      }
+    ]);
+  }
+
   return (
     <>
       <Header title="Todo App dentro da prop" />
+      <InputTodo aoAdicionarTodo={adicionarTodo} />
+
+      <FlatList 
+        data={todos} 
+        keyExtractor={todo=> todo.id}
+        renderItem={({item}) => (
+          <Text>{item.value}</Text>
+        )}
+      />
     </>
   );
 };
---
+++ components/InputTodo.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+const InputTodo = (props) => {
+
+  const [textoTodo, setTextoTodo] = useState('');
+  
+  const aoDigitarNoInput = (textoInput) => {
+    setTextoTodo(textoInput)
+  }
+
+  const adicionarTodo = () => {
+    props.aoAdicionarTodo(textoTodo)
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput 
+        placeholder="Digite um todo" 
+        style={styles.inputTodo}
+        onChangeText={aoDigitarNoInput}
+      />
+      
+      <TouchableOpacity
+        style={styles.botaoTodo}
+        onPress={adicionarTodo}
+      >
+        <Text style={styles.botaoTexto}>Adicionar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+/* estilos */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputTodo: {
+      borderWidth: 1,
+      borderColor: 'purple',
+      width: '80%',
+      padding: 10
+  },
+  botaoTodo: {
+    backgroundColor: '#8B10AE',
+    padding: 10
+  },
+  botaoTexto: {
+    color: '#fff',
+    fontSize: 16
+  }
+});
+
+export default InputTodo;
